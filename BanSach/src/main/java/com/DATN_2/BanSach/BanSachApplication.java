a2f21b23ec10e7f045da60f37d6c051753e7daa5
namnh191103 <namnh191103@gmail.com>
2025-05-07 12:29:02 +0700

    Initial commit

---
+++ BanSach/src/main/java/com/DATN_2/BanSach/BanSachApplication.java
@@ -0,0 +1,13 @@
+package com.DATN_2.BanSach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanSachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanSachApplication.class, args);
+	}
+
+}
---
+++ BanSach/src/test/java/com/DATN_2/BanSach/BanSachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DATN_2.BanSach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanSachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
